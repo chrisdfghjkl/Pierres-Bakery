513c36c7910d8fbbe6572832e6aa06996e15c04f
chrisdfghjkl <cdepastene@gmail.com>
2022-02-11 13:38:04 -0800

    add second test (already passing)

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -14,5 +14,13 @@ namespace Bakery.Tests
       Bread newBread = new Bread(2);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+
+    [TestMethod]
+    public void LoafCount_ReturnsNumberOfLoavesPurchased_Int()
+    {
+      int loafCount = 2;
+      Bread newBread = new Bread(loafCount);
+      Assert.AreEqual(loafCount, newBread.LoafCount);
+    }
   }
 } 
