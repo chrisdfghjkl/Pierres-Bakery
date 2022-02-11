5503061758223b607cb1fb75fc912f9caf5ec136
chrisdfghjkl <cdepastene@gmail.com>
2022-02-10 23:59:21 -0800

    add first test

---
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void Bread_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread(2);
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+  }
+} 
