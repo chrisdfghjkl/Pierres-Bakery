89c6097494d838b0f81f948e7bf82dc442d6c8ad
chrisdfghjkl <cdepastene@gmail.com>
2022-02-10 23:06:46 -0800

    add title information

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -5,6 +5,19 @@ namespace Bakery
 {
   public class Program
   {
-
+    public static void Main()
+    {
+      Console.WriteLine("==============================");
+      Console.WriteLine("| Welcome to Pierre's Bakery |");
+      Console.WriteLine("|                            |");
+      Console.WriteLine("|       Bread: $5 each       |");
+      Console.WriteLine("|      Pastries: $5 each     |");
+      Console.WriteLine("|                            |");
+      Console.WriteLine("|      Current Specials:     |");
+      Console.WriteLine("|  Bread - Buy 2 Get 1 Free  |");
+      Console.WriteLine("|      Pastry - 3 For $5     |");
+      Console.WriteLine("|                            |");
+      Console.WriteLine("==============================");
+    }
   }
 }
