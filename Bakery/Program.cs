663ae9f28e44e02ff3aa8585624432263a0b86e3
chrisdfghjkl <cdepastene@gmail.com>
2022-02-12 01:43:16 -0800

    add remaining UI logic

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -23,12 +23,30 @@ namespace Bakery
       Console.WriteLine("Welcome to Pierre's, how many loaves of bread would you like?");
       string inputBreadOrder = Console.ReadLine();
       int LoafCount = int.Parse(inputBreadOrder);
-      Bread breadOrder = new Bread(loafCount);
+      Bread breadOrder = new Bread(LoafCount);
+      Console.WriteLine("");
 
       Console.WriteLine("You got it! Now, how many pastries would you like?");
       string inputPastryOrder = Console.ReadLine();
       int PastryCount = int.Parse(inputPastryOrder);
-      Pastry pastryOrder = new Pastry(pastryCount);
+      Pastry pastryOrder = new Pastry(PastryCount);
+      Console.WriteLine("");
+
+      int orderTotal = breadOrder.BreadCost() + pastryOrder.PastryCost();
+
+      Console.WriteLine("Thanks! Your total is $" + orderTotal);
+      Console.WriteLine("");
+      Console.WriteLine("Would you like to place another order? [Y/N]");
+      string userInput = Console.ReadLine();
+      if (userInput == "Y" || userInput == "y")
+      {
+        Main();
+      }
+      else
+      {
+        Console.WriteLine("Have a good day!");
+      }
+      
 
 
     }
